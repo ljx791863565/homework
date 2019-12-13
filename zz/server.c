8c6ed06dda15f451bfae514173d651526ab0552e
ljx <liaojia138797@163.com>
2019-12-13 19:32:11 +0800

    131219 at company

--- zz/server.c
+++ zz/server.c
@@ -1,144 +1,128 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include "unp.h"
 
-#define BUF_SIZE 1024
-#define MAX_LISTEN 20
-#define MAX_EVENT 100
-void* worker(void *arg)
+int main(int argc, char *argv[])
 {
-	int fd = *((int *)arg);
-	char buf[BUF_SIZE];
-	int ret;
-	while (1){
-		memset(buf, 0, sizeof(buf));
-		ret = read(0, buf, sizeof(buf));
-		if (ret < 0){
-			perror("read");
-			return NULL;
-		}
-		ret = write(fd, buf, ret-1);
-		if (ret < 0){
-			perror("write");
-			return NULL;
-		}
-		printf("server to client: %s\n", buf);
-	}
-}
-int main(int argc, char **argv)
-{
-	if (argc != 2){
-		printf("usage: ./a.out port\n");
+	if (argc != 3){
+		printf("Usage : %s ip port \n", argv[0]);
 		return -1;
 	}
-
-	struct sockaddr_in clientAddr;
-	const char const *p = "127.0.0.1";
-	memset(&clientAddr, 0, sizeof(clientAddr));
-	clientAddr.sin_family = AF_INET;
-	clientAddr.sin_port = htons(atoi(argv[1]));
-	clientAddr.sin_addr.s_addr = inet_addr(p);
-
+	
+	struct sockaddr_in serverAddr;
+	bzero(&serverAddr, 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(argv[2]));
+	serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0){
+	if (sockfd == -1){
 		perror("socket");
 		return -1;
 	}
 
-	int ret = bind(sockfd, (struct sockaddr*)&clientAddr, sizeof(clientAddr));
-	if (ret < 0){
+	
+	int option = 1;
+	int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char *)&option, sizeof(option));
+	if (ret == -1){
+		perror("setsockopt");
+		return -1;
+	}
+
+	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if (ret == -1){
 		perror("bind");
 		return -1;
 	}
 	
 	ret = listen(sockfd, MAX_LISTEN);
-	if (ret < 0){
+	if (ret == -1){
 		perror("listen");
 		return -1;
+	}	
+
+	int epfd = epoll_create(MAX_EVENT);
+	if (epfd == -1){
+		perror("epoll_create");
+		return -1;
 	}
-
-	struct sockaddr_in serverAddr;
-	socklen_t serlen;
-
-	int epfd;
 	struct epoll_event ev, event[MAX_EVENT];
-
-	epfd = epoll_create(MAX_EVENT);
-	if (epfd < 0){
-		perror("event_create");
-		return -1;
-	}
 	ev.data.fd = sockfd;
-	ev.events = EPOLLIN | EPOLLET;
+	ev.events = EPOLLIN;
 	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
-	if (ret < 0){
-		perror("event_ctl");
+	if (ret == -1){
+		perror("epoll_ctl");
 		return -1;
-	}
+	}	
 
-	int wait_count = 0;
-	int i = 0;
-	char buf[BUF_SIZE];
-	int accpFd, readFd, writeFd;
-	pthread_t pid;
-	while (1){
+	struct sockaddr_in clientAddr;
+	bzero(&clientAddr, 0);
+	socklen_t cli_len = sizeof(clientAddr);
+	int listenFd, readFd, writeFd;
+	int wait_count;
+	int i;
+	char buf[BUFSIZE];
+	while (1)
+	{
 		wait_count = epoll_wait(epfd, event, MAX_EVENT, 500);
-
+		if (wait_count == -1){
+			perror("epoll_wait");
+			if (errno == EINTR)
+				continue;
+		}
 		for (i = 0; i < wait_count; i++){
-			if (event[i].data.fd == sockfd){
-				accpFd = accept(sockfd, (struct sockaddr*)&serverAddr, &serlen);
-				if (accpFd < 0){
-					perror("accept");
-					return -1;
-				}
-				printf("A new client connecttion is ok\n");
-				ev.data.fd = accpFd;
-				ev.events = EPOLLIN | EPOLLET;
-				ret = epoll_ctl(epfd, EPOLL_CTL_ADD, accpFd, &ev);
+			if ((event[i].events & EPOLLERR) || (event[i].events & EPOLLHUP) || (!(event[i].events & EPOLLIN))){
+				close(event[i].data.fd);
+				continue;	
+			}else if (sockfd == event[i].data.fd){
+			//	while (1){
+					listenFd = accept(sockfd, (struct sockaddr*)&clientAddr, &cli_len);
+					if (listenFd == -1){
+						if ((errno == EAGAIN) || (errno == EWOULDBLOCK)){
+							continue;
+						}else {
+							perror("accept");
+							continue;
+						}	
+					}
+					printf("a new client connected successfully\n");
+					ev.data.fd = listenFd;
+					ev.events = EPOLLIN | EPOLLET;
+					ret = epoll_ctl(epfd, EPOLL_CTL_ADD, listenFd, &ev);
+			//	}
 			}else if (event[i].events & EPOLLIN){
-				memset(buf, 0, sizeof(buf));
 				readFd = event[i].data.fd;
+				bzero(buf, 0);
 				ret = read(readFd, buf, sizeof(buf));
-				if (ret < 0){
-					perror("read");
+				if (ret == -1){
+					if (errno == ECONNRESET){
+						close(readFd);
+						epoll_ctl(epfd, EPOLL_CTL_DEL, readFd, &ev);
+						event[i].data.fd = -1;
+					}
+						continue;
+				}else if (ret == 0){
+					close(readFd);
+					epoll_ctl(epfd, EPOLL_CTL_DEL, readFd, &ev);
+					event[i].data.fd = -1;
+					continue;
 				}
-				printf("from client : %s\n", buf);
-				
-				ret = pthread_create(&pid, 0, worker, &readFd);
-				if (ret < 0){
-					perror("pthread_create");
-					return -1;
-				}
-				
-			}
+				ev.data.fd = readFd;
+				ev.events = EPOLLOUT | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, readFd, &ev);
+				printf("server read msg:%s\n", buf);
+			}else if (event[i].events & EPOLLOUT){
+				writeFd = event[i].data.fd;
+				bzero(buf, 0);
+				ret = read(0, buf, sizeof(buf));
+				ret = write(writeFd, buf, sizeof(buf));
+				ev.data.fd = writeFd;
+				ev.events = EPOLLOUT | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, writeFd, &ev);
+			}	
 		}
 	}
-
-	/*
-
-	char buf[BUF_SIZE];
-
-	while (1){
-		memset(buf, 0, sizeof(buf));
-		ret = read(accpFd, buf, sizeof(buf));
-		if (ret < 0){
-			perror("read");
-			return -1;
-		}
-
-		printf("from client: %s\n", buf);
-	}
-	*/	
-	pthread_join(pid, NULL);
-
-	close(epfd);
-	close(sockfd);
+		close(sockfd);
+		close(epfd);
+		close(listenFd);
 	return 0;
 
 }
